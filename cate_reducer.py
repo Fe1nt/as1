6c391e17fa8a6f8054648e7c0dd3728f25f737ce
Fe1nt <andystar141@outlook.com>
2019-04-07 00:29:31 +1100

    7

--- cate_reducer.py
+++ cate_reducer.py
@@ -10,43 +10,43 @@ def cate_reducer():
     #for category, video_id, country in read_map_output(sys.stdin):
     for category, videoID, countryID in read_map_output(sys.stdin):
 
-        if category=="category": # ignore the first row
+        if videoID=="video_id": # ignore the first row
             continue
-        current_category =category
-        category_count.append[videoID,countryID]
         if category != current_category:
-            category_count = list(set([tuple(t) for t in category_count]))
-            category_count = [list(v) for v in category_count]
-            numCountry = len(category_count)
-            list1 =[]
-            for item in category_count:
-                video =item[0]
-                list1.append[video]
-            list2 = []
-            for id in list1:
-                if id not in list2:
-                    list2.append(id)
-            numVideo = len(list2)
-            average =str(("%.2f" %(numCountry/numVideo)))
-            print("{}\t{}".format(current_category, average))
-            current_category = category
+            if current_category != ""：
+                category_count = list(set([tuple(t) for t in category_count]))
+                category_count = [list(v) for v in category_count]
+                numCountry = len(category_count)
+                list1 =[]
+                for item in category_count:
+                    video =item[0]
+                    list1.append[video]
+                list2 = []
+                for id in list1:
+                    if id not in list2:
+                        list2.append(id)
+                numVideo = len(list2)
+                average =str(("%.2f" %(numCountry/numVideo)))
+                print("{}\t{}".format(current_category, average).strip())
+                
             category_count = []
-    category_count = list(set([tuple(t) for t in category_count]))
-    category_count = [list(v) for v in category_count]
-    numCountry = len(category_count)
-    list1 =[]
-    for item in category_count:
-        video =item[0]
-        list1.append[video]
-    list2 = []
-    for id in list1:
-        if  id not in list2:
-            list2.append(id)
-    numVideo = len(list2)
-    average =str(("%.2f" %(numCountry/numVideo)))
-    print("{}\t{}".format(current_category, average))
-    current_category = ""
-    category_count = []
+            current_category =category
+        category_count.append[videoID,countryID]
+    if current_category != ""：            
+        category_count = list(set([tuple(t) for t in category_count]))
+        category_count = [list(v) for v in category_count]
+        numCountry = len(category_count)
+        list1 =[]
+        for item in category_count:
+            video =item[0]
+            list1.append[video]
+        list2 = []
+        for id in list1:
+            if  id not in list2:
+                list2.append(id)
+        numVideo = len(list2)
+        average =str(("%.2f" %(numCountry/numVideo)))
+        print("{}\t{}".format(current_category, average).strip())
    # for the last category, an output action after for loop ends is necessary
    
 
